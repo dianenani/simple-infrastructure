199116fbae26ebb0d92e93dbd864ccb7640e1732
Diane Newton <dn7625@gmail.com>
2025-11-22 00:25:10 -0500

    Add Terraform configs for VPC, subnets, EC2, and ALB

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo su
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "<html><head><style>body { background-color: purple; color: white; }</style></head><body><h1><p> Welcome to my First Project. This is Diane and I am a happy student.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></body></html>" > /var/www/html/index.html
+ 
